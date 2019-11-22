a668a4a807f279bdbba5e0b0de6e4e35fab73de5
Илья <33377392+savei932@users.noreply.github.com>
2019-11-22 16:29:53 +0700

    logger.php
    Более подробная информация

--- logger/logger.php
+++ logger/logger.php
@@ -6,7 +6,24 @@ class logger {
      * Выполняется при подключение модуля
      */
     static function execute () {
-        file_put_contents("log",file_get_contents('log') . '---------->' . logger::getTimeSession() . "\n" . "[ip] => " . logger::getIp() . "\n" . logger::getRequest_uri() . "\n" . logger::getUserAgent() . "\n");
+        $xlib = new xlib();
+        $path = $_SERVER['DOCUMENT_ROOT'] . $xlib->getPathModules('logger');
+        if (!is_writable($path)) {
+            echo "записи нет у этого файла $path";
+            echo "</br>Пожалуйста укажите разрешение на запись в вашей системы!";
+            echo "</br>sudo chmod 0777 $path";
+            echo "</br>" . $xlib->getPathModules('logger');
+            chmod($xlib->getPathModules('logger'), 0777);
+            die();
+        }
+        chmod("$path/log", 0727);
+        file_put_contents("$path/log", 
+            file_get_contents("$path/log") . '---------->' . logger::getTimeSession() . "\n" .
+            logger::getIp() . "\n" .
+            logger::getRequest_uri() . "\n" .
+            logger::getUserAgent() . "\n"
+        );
+        chmod("$path/log", 0222);
     }
 
     /**
@@ -20,7 +37,7 @@ class logger {
         if(filter_var($client, FILTER_VALIDATE_IP)) $ip = $client;
         elseif(filter_var($forward, FILTER_VALIDATE_IP)) $ip = $forward;
         else $ip = $remote;
-        return $ip;
+        return "[ip] => " $ip;
     }
 
     /**
